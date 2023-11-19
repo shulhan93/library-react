d3e3822f4c9b02c142e8635337fbe9e848c3a31d
shulgan-andrei <eng.shulhan.andrei@gmail.com>
2023-11-19 13:00:46 +0300

    Added BookForm, BookList and Filter components

---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,8 @@
+import "./BookList.css";
+export default function BookList() {
+  return (
+    <div className="app-block book-list">
+      <h2>Book List</h2>
+    </div>
+  );
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,9 @@
+import "./Filter.css";
+
+export default function Filter() {
+  return (
+    <div className="app-block filter">
+      <h2>Filter</h2>
+    </div>
+  );
+}
